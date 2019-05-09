d0b0e6865f8bb639f22a370ed98e18b99ce35358
Guang_Long Ma <33285642+maguanglong@users.noreply.github.com>
2019-05-09 17:29:46 +0800

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pandas import Series,DataFrame
 def describe_data(data,ifprint):
     out={'method':['count','min','max','argmin','argmax','idxmin','idxmax','quantile',
@@ -27,3 +28,34 @@ def describe_data(data,ifprint):
         print(dfout)
     
     return dfout
+
+def woe_var(data, var_name, status_name, nbins):
+    #var_name="age"
+    status=data[status_name]
+    xvar=data[var_name]
+    xmax=xvar.max()
+    xmin=xvar.min()
+    bad = status.sum()
+    good = status.count() - bad
+    d1 = pd.DataFrame({"status": status, var_name: xvar, "Bucket": pd.qcut(xvar, nbins)})
+    d2 = d1.groupby('Bucket', as_index = True)
+    woe_list = pd.DataFrame(d2[var_name].min(), columns = ['min'])
+    woe_list['min']=d2[var_name].min()
+    woe_list['max'] = d2[var_name].max()
+    woe_list['total'] = d2.count().status
+    woe_list['bad']=d2.sum().status
+    woe_list['good']=woe_list['total']-woe_list['bad']
+    woe_list['rate'] = d2.mean().status #=woe_list['bad']/woe_list['total']
+    woe_list['woe']=np.log((woe_list['bad']/bad)/(woe_list['good']/good))
+    woe_list['iv_i']=(woe_list['good']/good-woe_list['bad']/bad)*woe_list['woe']
+    iv=woe_list['iv_i'].sum()
+    woe_list['iv']=iv
+    print(woe_list)
+    print("="*66)
+    mapping=[]
+    mapping.append(xmin)
+    for i in range(1,nbins+1):
+        qua=xvar.quantile(i/(nbins+1))
+        mapping.append(round(qua,4))
+    mapping.append(xmax)
+    return woe_list,mapping
