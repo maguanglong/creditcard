3711a71f0e68aa7c352c77b611078ad7837b5066
Guang_Long Ma <33285642+maguanglong@users.noreply.github.com>
2019-05-09 18:17:52 +0800

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,5 +1,14 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 11:04:54 2019
+
+@author: guanglong ma
+"""
 from pandas import Series,DataFrame
+import numpy as np
+import pandas as pd
+
+
 def describe_data(data,ifprint):
     out={'method':['count','min','max','argmin','argmax','idxmin','idxmax','quantile',
                'sum','mean','median','mad','var','std','skew','kurt']}
@@ -37,7 +46,10 @@ def woe_var(data, var_name, status_name, nbins):
     xmin=xvar.min()
     bad = status.sum()
     good = status.count() - bad
-    d1 = pd.DataFrame({"status": status, var_name: xvar, "Bucket": pd.qcut(xvar, nbins)})
+    if len(xvar.unique())<50:
+        d1 = pd.DataFrame({"status": status, var_name: xvar, "Bucket": pd.cut(xvar, nbins)})
+    else:
+        d1 = pd.DataFrame({"status": status, var_name: xvar, "Bucket": pd.qcut(xvar, nbins)})
     d2 = d1.groupby('Bucket', as_index = True)
     woe_list = pd.DataFrame(d2[var_name].min(), columns = ['min'])
     woe_list['min']=d2[var_name].min()
@@ -46,6 +58,8 @@ def woe_var(data, var_name, status_name, nbins):
     woe_list['bad']=d2.sum().status
     woe_list['good']=woe_list['total']-woe_list['bad']
     woe_list['rate'] = d2.mean().status #=woe_list['bad']/woe_list['total']
+    #woe_list['sum'] = d2.sum().status
+    #woe_list['rate'] = d2.mean().status
     woe_list['woe']=np.log((woe_list['bad']/bad)/(woe_list['good']/good))
     woe_list['iv_i']=(woe_list['good']/good-woe_list['bad']/bad)*woe_list['woe']
     iv=woe_list['iv_i'].sum()
