1a95aed6734542753f190471c8478bc909d4ec07
Guang_Long Ma <33285642+maguanglong@users.noreply.github.com>
2019-05-10 18:13:07 +0800

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -73,3 +73,58 @@ def woe_var(data, var_name, status_name, nbins):
         mapping.append(round(qua,4))
     mapping.append(xmax)
     return woe_list,mapping
+
+# 异常值处理
+def outlier_processing(data,var_name):
+    s=data[var_name]
+    oneQuoter=s.quantile(0.25)
+    threeQuote=s.quantile(0.75)
+    irq=threeQuote-oneQuoter
+    xmin=oneQuoter-1.5*irq
+    xmax=threeQuote+1.5*irq
+    data=data[data[var_name]<=xmax]
+    data=data[data[var_name]>=xmin]
+    return data
+#######################################################################################################################
+#####################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+    
+# 载入数据
+data=pd.read_csv('cs-training.csv')
+# 数据集确实和分布情况
+data.describe().to_csv('DataDescribe.csv')
+if data.keys()[0]=='Unnamed: 0':
+    del data['Unnamed: 0']
+
+varlist=data.keys()
+#delet the missing value
+data=data.dropna()
+dfout=describe_data(data,0)
+ds=outlier_processing(data,"NumberOfTime60-89DaysPastDueNotWorse")
+status_name='SeriousDlqin2yrs'
+nbins=10;
+for var_name in varlist:
+    if var_name != status_name:
+        print("="*5+var_name+"="*5)
+        data = outlier_processing(data,var_name)
+        df=data[var_name]
+        status_name="SeriousDlqin2yrs"
+        
+        
+        
+        
+        #按四分位数切割
+        #df=pd.qcut(df,10)
+        dp=pd.crosstab(df,data[status_name])
+        
+        
+        dp.loc[:,'total']=dp.loc[:,1]+dp.loc[:,0]
+        dp.loc[:,'P_total']=dp.loc[:,'total']/df.count()
+        dp.loc[:,'P_cum']=dp.loc[:,'P_total'].cumsum()
+        dp.loc[:,'P_good']=dp.loc[:,0]/dp.loc[:,'total']
+        dp.loc[:,'P_bad']=dp.loc[:,1]/dp.loc[:,'total']
+        dp.loc[:,'P']=dp.loc[:,'P_good']/dp.loc[:,'P_bad']
+        print(dp)
+
+#        woelist, mapping = woe_var(data,var_name,status_name,nbins)
